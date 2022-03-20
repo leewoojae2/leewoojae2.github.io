da8562f85da79afc99401a2e2f3eeae522871357
woojaelee <euj9121@naver.com>
2022-03-20 21:09:00 +0900

    correction

--- goal/js/back.js
+++ goal/js/back.js
@@ -2,7 +2,7 @@ function random() {
   const imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
   const randomimg = imgs[Math.floor(Math.random() * imgs.length)];
   const body = document.querySelector("body");
-  const background = `img/${randomimg}`;
+  const background = `goal/img/${randomimg}`;
   body.style.backgroundImage = "url(" + background + ")";
 }
 random();
