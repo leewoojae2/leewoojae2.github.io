f1e1e756bfa81f9ac845879ac93ee679a157465c
woojaelee <euj9121@naver.com>
2022-03-29 00:54:58 +0900

    move

--- goal/js/weather.js
+++ goal/js/weather.js
@@ -3,6 +3,7 @@ const API_KEY = "b5f4b9e54989391943e92ecef6a9d634";
 function ok(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
+
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
